a44d7b3a7849a741f0f3e281325aad8728510262
dorkmo <dorkmo@daum.net>
2020-07-02 17:41:00 -0500

    Create Stepper-BigEasyDriver-MicroStep.ino
    constant micro stepping with big easy driver

---
+++ Arduino/Stepper-MotorShield/Stepper-BigEasyDriver-MicroStep.ino
@@ -0,0 +1,62 @@
+/****************************************************************************** 
+SparkFun Big Easy Driver Basic Demo
+Toni Klopfenstein @ SparkFun Electronics
+February 2015
+https://github.com/sparkfun/Big_Easy_Driver
+
+Simple demo sketch to demonstrate how 5 digital pins can drive a bipolar stepper motor,
+using the Big Easy Driver (https://www.sparkfun.com/products/12859). Also shows the ability to change
+microstep size, and direction of motor movement.
+
+Development environment specifics:
+Written in Arduino 1.6.0
+
+This code is beerware; if you see me (or any other SparkFun employee) at the local, and you've found our code helpful, please buy us a round!
+Distributed as-is; no warranty is given.
+
+Example based off of demos by Brian Schmalz (designer of the Big Easy Driver).
+http://www.schmalzhaus.com/EasyDriver/Examples/EasyDriverExamples.html
+******************************************************************************/
+//Declare pin functions on Arduino
+#define stp 2
+#define dir 3
+#define MS1 4
+#define MS2 5
+#define MS3 6
+#define EN  7
+
+//Declare variables for functions
+char user_input;
+int x;
+int y;
+int state;
+
+void setup() {
+  pinMode(stp, OUTPUT);
+  pinMode(dir, OUTPUT);
+  pinMode(MS1, OUTPUT);
+  pinMode(MS2, OUTPUT);
+  pinMode(MS3, OUTPUT);
+  pinMode(EN, OUTPUT);
+  
+  digitalWrite(stp, LOW);
+  digitalWrite(dir, LOW);
+  digitalWrite(MS1, LOW);
+  digitalWrite(MS2, LOW);
+  digitalWrite(MS3, LOW);
+  digitalWrite(EN, HIGH);
+
+  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
+  digitalWrite(MS1, HIGH); //Pull MS1,MS2, and MS3 high to set logic to 1/16th microstep resolution
+  digitalWrite(MS2, HIGH);
+  digitalWrite(MS3, HIGH);
+  
+  }
+
+//Main loop
+void loop() {
+    digitalWrite(stp,HIGH); //Trigger one step forward
+    delay(1);
+    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    delay(1);
+}
